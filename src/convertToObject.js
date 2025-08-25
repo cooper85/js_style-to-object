277b27deec3fd25c2367628dd57370d2b4ac9b5c
Sergey Zubinskiy <sergey.zubinskiy@skynix.co>
2025-08-25 23:55:00 +0300

    implementation

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,18 @@
 'use strict';
 
 /**
- * @param {string} sourceString
- *
- * @return {object}
+ * @param {string} stylesString
+ * @returns {Object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(stylesString) {
+  return stylesString
+    .split('\n')
+    .filter(line => line.trim() && line.includes(':')) // оставляем только строки с :
+    .reduce((acc, line) => {
+      const [property, ...valueParts] = line.replace(/;$/, '').split(':');
+      acc[property.trim()] = valueParts.join(':').trim();
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;
